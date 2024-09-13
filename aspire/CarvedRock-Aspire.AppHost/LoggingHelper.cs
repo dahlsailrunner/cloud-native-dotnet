b2ed77c89960f2cca8efe665ec7beffe6248e5f6
Erik Dahl <erik.dahl@realpage.com>
2024-09-13 06:16:27 -0500

    minor updates to show shared logging levels

---
+++ aspire/CarvedRock-Aspire.AppHost/LoggingHelper.cs
@@ -0,0 +1,28 @@
+﻿namespace Aspire.Hosting;
+
+internal static class LoggingHelper
+{
+    internal static IResourceBuilder<T> WithSharedLoggingLevels<T>(this IResourceBuilder<T> builder)
+        where T : IResourceWithEnvironment
+    {
+        var dict = new Dictionary<string, string>
+        {
+            { "Default", "Information" },
+            { "Microsoft", "Warning" },
+            { "Microsoft.Hosting", "Information" },
+            { "Microsoft.EntityFrameworkCore.Database.Command", "Information" },
+            { "Microsoft.EntityFrameworkCore.Query", "Information" },
+            { "Microsoft.EntityFrameworkCore.Update", "Information" },
+        };
+
+        if (dict.ContainsKey("Default"))
+        {
+            builder = builder.WithEnvironment($"Serilog__MinimumLevel__Default", dict["Default"]);
+        }
+        foreach (var item in dict.Keys.Where(k => k != "Default"))
+        {
+           builder = builder.WithEnvironment($"Serilog__MinimumLevel__Override__{item}", dict[item]);
+        }
+        return builder;
+    }
+}
--- aspire/CarvedRock-Aspire.AppHost/Program.cs
+++ aspire/CarvedRock-Aspire.AppHost/Program.cs
@@ -5,15 +5,19 @@ var carvedrockdb = builder.AddPostgres("postgres")
 
 var emailService = builder.AddSmtp4Dev("SmtpUri");  // custom extension
 
-var identityserver = builder.AddProject<Projects.CarvedRock_Identity>("carvedrock-identity");
+var identityserver = builder.AddProject<Projects.CarvedRock_Identity>("carvedrock-identity")
+    .WithSharedLoggingLevels();
+
 var identityEndpoint = identityserver.GetEndpoint("https");
 
 var api = builder.AddProject<Projects.CarvedRock_Api>("carvedrock-api")
     .WithReference(carvedrockdb)
+    .WithSharedLoggingLevels()
     .WithEnvironment("Auth__Authority", identityEndpoint);
 
 builder.AddProject<Projects.CarvedRock_WebApp>("carvedrock-webapp")
     .WithReference(api)    
+    .WithSharedLoggingLevels()
     .WithEnvironment("Auth__Authority", identityEndpoint)
     .WithReference(emailService);    
 
